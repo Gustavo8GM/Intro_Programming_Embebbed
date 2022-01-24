697577a0043b55bfc461f3f0f1e2350874e1d761
Gustavo8GM <2109112@upy.edu.mx>
2022-01-24 09:44:42 -0600

    First commit

---
+++ Unit 1/Hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //Importar la biblioteca
+
+int main(){
+
+    printf("Hello world");
+
+    return 0;
+
+}
---
+++ Unit 1/Sumas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Ingrese el primer numero");
+
+    read->a;
+    
+    printf("Ingrese su segundo numero");
+    read->b;
+
+    int sum (a+b);
+    read->c;
+    
+    printf("El resultado de su suma es" " " c );
+
+    return 0;
+}

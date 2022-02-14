f8e783be933b53a60524db8cc17fc8b79fea807c
Gustavo8GM <2109112@upy.edu.mx>
2022-02-14 08:19:14 -0600

    code finish

--- Unit 1/Variables.c
+++ Unit 1/Variables.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+
+//Datatype:
+//create a variable datatype anyName | 
+
+int myint = 24; //asi se indica la variable entera
+float myfloat = 22.7; //variable con punto decimal
+char mychar = '1'; //formato de texto %c
+int myarray [7] = {5,27,43,12,8,7,0};
+               //  0  1  2  3 4 5 6...n
+
+
+float myfloatarray [255] = {1.33, 2.55, 666.1};
+;char word [7] = {'G', 'U', 'S', 'T', 'A', 'V', 'O'};
+char country [10] = 'Mexico'; //%s
+char aName = 'Gustavo';
+
 int main() {
+    printf {"imprimir valor, myint = %d\n", myint};
+    
+    printf {"hola clase"}
+    
 
 
     return 0;

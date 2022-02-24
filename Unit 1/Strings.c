189f752ee36464c1eea5d307cf621cdc92b0ac55
Gustavo8GM <2109112@upy.edu.mx>
2022-02-24 08:36:54 -0600

    activity 4

--- Unit 1/Strings.c
+++ Unit 1/Strings.c
@@ -3,11 +3,18 @@
 
 
 char str[] = " ";
+char lastname[10] = {'G', 'u', 's', 't', 'a', 'v', 'o'};
+
+
 
 int main(int argc , char** argv){
 
     strcpy(str,argv[1]);
     printf("%s\n", str);
+    strcpy(lastname, argv[3]);
+    printf("Letra del nombre %s\n", lastname);
+
+
 
 
     return 0;

29c937d4b8948469d3ae73f76c69cff103c70be9
Gustavo8GM <2109112@upy.edu.mx>
2022-03-07 07:28:02 -0600

    numeros pares e impares

---
+++ Unit 1/inputsname.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+int main(int argc, char** argv){
+
+
+
+    printf("argc[0]: %s, argv1: %s, argv2: %s\n", argv[0], argv[1], argv[2], argv[3]);
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
